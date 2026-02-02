1a4de0ef5b9146d23f81bfdaf9f2b95e60b09b0e
TerryLuschen <Terry.Luschen@hotmail.com>
2026-02-02 14:11:51 -0600

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -106,6 +106,8 @@
     activity.metaData = activity.metaData || {};
     activity.metaData.isConfigured = true;
 
+    console.log('payload saving:', JSON.stringify(activity, null, 2));
+
     // Tell Journey Builder to save this configuration
     connection.trigger('updateActivity', activity);
   }
